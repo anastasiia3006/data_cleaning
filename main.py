c681eb5f3947c3dd1d4e0c1ad5b422c3171b69b0
anastasiia3006 <108720682+anastasiia3006@users.noreply.github.com>
2025-01-28 14:39:15 +0000

    create

---
+++ main.py
@@ -0,0 +1,15 @@
+from ucimlrepo import fetch_ucirepo 
+import pandas as pd
+
+# fetch dataset 
+adult = fetch_ucirepo(id=2) 
+  
+# data (as pandas dataframes) 
+X = adult.data.features 
+y = adult.data.targets 
+  
+data = pd.concat([X, y], axis=1)
+
+data.to_csv('adult_data.csv')
+
+print(data.head())
