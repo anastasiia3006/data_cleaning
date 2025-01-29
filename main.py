97bfd3e1b4b0337a76de60b32c6a929d9c424e6b
anastasiia3006 <108720682+anastasiia3006@users.noreply.github.com>
2025-01-29 15:24:58 +0100

    now

--- main.py
+++ main.py
@@ -18,27 +18,27 @@ data.to_csv('adult_data.csv')
 df = pd.read_csv('adult_data.csv')
 
 # create new file with all information about a DB
-#check_DB(df=df)
+check_DB(df=df)
 
 #  Processes emissions in the specified numerical and categorical columns.
 
-# categorical_columns = df.select_dtypes(include=['object']).columns.tolist()
-# numerical_columns = df.select_dtypes(include=['number']).columns.tolist()
+categorical_columns = df.select_dtypes(include=['object']).columns.tolist()
+numerical_columns = df.select_dtypes(include=['number']).columns.tolist()
 #
-# categorical_df = pd.DataFrame({'Categorical Columns': categorical_columns })
-# numerical_df = pd.DataFrame({'Numerical Columns': numerical_columns})
+categorical_df = pd.DataFrame({'Categorical Columns': categorical_columns })
+numerical_df = pd.DataFrame({'Numerical Columns': numerical_columns})
 #
-# categorical_file = 'categorical_col.csv'
-# numerical_file = 'numerical_col.csv'
-#
-# categorical_df.to_csv(categorical_file, index = False)
-# numerical_df.to_csv(numerical_file, index=False)
+categorical_file = 'categorical_col.csv'
+numerical_file = 'numerical_col.csv'
+
+categorical_df.to_csv(categorical_file, index = False)
+numerical_df.to_csv(numerical_file, index=False)
 
 df_categorical = pd.read_csv('categorical_col.csv')['Categorical Columns'].dropna().tolist()
 df_numerical = pd.read_csv('numerical_col.csv')['Numerical Columns'].dropna().tolist()
 
 # start processing emissions
-#df_clean_numerical = handle_outliers(df, numerical_columns)
+df_clean_numerical = handle_outliers(df, numerical_columns)
 #print(df_clean_numerical.info())
 #print(df_clean_numerical.describe())
 
